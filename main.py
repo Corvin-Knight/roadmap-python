9e5cc531d75360942948dc8d026db92fe77cc31d
Solo <bekeen75@gmail.com>
2026-03-19 22:36:59 -0400

    Added Lesson 1: Variables and f-strings

--- main.py
+++ main.py
@@ -7,3 +7,31 @@ print("Status: Ready for roadmap.sh development")
 tools = ["Cursor", "Git Bash", "GitHub Sync"]
 for tool in tools:
     print(f"Configured: {tool}")
+
+
+
+
+
+
+    # ================================
+    # LESSON 1: VARIABLES & DATA TYPES
+    # ================================
+
+    # String (Text)
+    coder_name = "Solo"
+
+    # Integer (Whole Numbers)
+    logic_points = 10
+
+    # Float (Decimal)
+    accuracy_percent = 98.5
+
+    # Boolean (True/False)
+    is_environment_ready = True
+
+    # --- THE CHALLENGE ---
+    # We use an 'f' before the quotes to "format" the string.
+    #It lets us drop variables into the string using { }
+    print(f"{coder_name} has {logic_points} points and an accuracy of {accuracy_percent}%")
+    print(f"Is the environment ready? {is_environment_ready}")
+    
