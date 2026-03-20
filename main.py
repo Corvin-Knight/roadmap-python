20c145c5469fdb83ba57a8b2341b387c06aa0e74
Solo <bekeen75@gmail.com>
2026-03-19 23:00:10 -0400

    Lesson 1: Inputs and Math complete

--- main.py
+++ main.py
@@ -34,4 +34,29 @@ for tool in tools:
     #It lets us drop variables into the string using { }
     print(f"{coder_name} has {logic_points} points and an accuracy of {accuracy_percent}%")
     print(f"Is the environment ready? {is_environment_ready}")
+
+
+
+    # --- MATH & UPDATING VARIABLES ---
+
+    # You can add to existing numbers
+    logic_points = logic_points + 5
+
+    # or use a shortcut called 'increment' (+=)
+    logic_points += 5
+    # You can even combine strings (Text)
+    status_update = "Progressing " + "Fast"
+    print(f"Updated Logic Points: {logic_points}")
+    print(f"Current Status: {status_update}")
+
+    # --- INTERACTIVE INPUT ---
+
+    # The input() command pasues the program and waits for the user to type something.
+    favorite_hobby = input("Enter a hobby you're currently into: ")
+
+    # Now we use that variable in a sentance
+    print(f"Adding {favorite_hobby} to Solo's profile...")
+    
+    # You can even 'concatenate' strings with a plus sign
+    print("Roadmap Lesson 1: " + "Complete.")
     
