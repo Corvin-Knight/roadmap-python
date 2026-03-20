21f27ed592f03fe59dab572136ac6a34ddb18bfc
Solo <bekeen75@gmail.com>
2026-03-20 19:14:31 -0400

    f-string syntax adjustment and cleaned hardware issue (keyboard)

---
+++ input_exercise.py
@@ -0,0 +1,4 @@
+name = input("What is your name? ")
+favorite_color = input("What is your favorite color? ")
+# print("Hello " , name +  ",", favorite_color , "is an awesome choice!")
+print(f"Hello {name}, {favorite_color} is an awesome choice!")
