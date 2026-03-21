aae06e8b2eb95d7e6aeb75719809c609d0594ad3
Solo <bekeen75@gmail.com>
2026-03-20 20:29:57 -0400

    Just so much, cheat sheet updated with int, type_conversion got rewritten with str to turn strings into integers into floats. whoa.

---
+++ cheat_sheet.py
@@ -0,0 +1,10 @@
+#  Whole numbers = Integers = int
+# Decimal numbers = float
+# Variable = "this is the data/string placed inside the 'box' that is " "variable""
+# input = tells the code to wait for user input before proceeding 
+# a string = text
+# ill add more later
+# "10" is a string, it is text. int(10) is a number, an integer, it is a number.
+# you have to tell python "10" is not a word, it is 10, a number, "10" + "10" is "1010"
+# int(10) + int(10) is int(20)
+# Python can only concatenate str (not "float") to str
---
+++ type_conversion.py
@@ -0,0 +1,28 @@
+# #  birth_year = input("Enter your birth year: ")
+# age = 2026 - int(birth_year)
+# print(age)
+
+# # int()
+# # float()
+# # bool()
+# # str() 
+
+# First: 10.5
+# Second: 15
+# Sum: 25.5
+
+
+# First: 10.1
+# Second: 20
+# First = input("First: ")
+# Second = input("Second: ")
+# sum = float(First) + float(Second)
+# print(sum)
+
+
+First: 10.1
+Second: 20
+First = float(input("First: "))
+Second = float(input("Second: "))
+sum = float(First) + float(Second)
+print("Sum: " + str(sum))
