b85b22f6a92285a34fe0907b1c270993a6fff889
Solo <bekeen75@gmail.com>
2026-03-21 20:11:26 -0400

    The weight converter can now accept lowercase answers and tells the user what the unit of measurment the answer is in.

--- if_statements_exercise.py
+++ if_statements_exercise.py
@@ -3,7 +3,9 @@ print("Simple Weight Converter")
 Weight = input("Weight: ")
 Unit = input("k(g) or l(bs): ")
  
-if Unit == "l":
+if Unit.upper() == "L":
+    print("Here is you Weight in Kilos!")
     print(float(Weight) * 0.453592) 
-elif Unit == "k":
-    print(float(Weight) / 0.453592)     
+elif Unit.upper() == "K":
+    print("Here is your Weight in Pounds!")
+    print(float(Weight) / 0.453592)
